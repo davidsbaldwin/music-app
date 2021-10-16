cdcd545fbeb563899cb96d54457acaba36957db3
Ameen Ilyas <=>
2021-10-16 21:46:00 +0500

    updated

--- validation/adminValidation.js
+++ validation/adminValidation.js
@@ -8,5 +8,4 @@ exports.singUpValidation = joi.object({
 exports.singInValidation = joi.object({
     email: joi.string().email().required(),
     password: joi.string().min(6).max(36).required(),
-    code: joi.string().min(24).max(24).required(),
 });
